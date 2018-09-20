a3e758e892ece202b44c03d4507664b215eba3c5
Shakhovichkirill <shakhovichkirill@gmail.com>
2018-09-21 00:40:10 +0300

    dz_1

---
+++ MySite/mainapp/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'mainapp/index.html')
+
+def catalog(request):
+        return render(request, 'mainapp/catalog.html')
+
+def contacts(request):
+        return render(request, 'mainapp/contacts.html')
+
+def product1(request):
+        return render(request, 'mainapp/product1.html')
+
+def product2(request):
+        return render(request, 'mainapp/product2.html')
+
+def product3(request):
+        return render(request, 'mainapp/product3.html')
+
